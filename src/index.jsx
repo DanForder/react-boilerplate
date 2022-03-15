e5624b8f53cab9ee4e05effaf7c6bc82d2643d00
Dan Forder <danfrdr@gmail.com>
2022-03-15 15:11:39 +0000

    updated baseurl

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const baseURL = "react-boilerplate";
+const baseURL = "/react-boilerplate";
 
 ReactDOM.render(
   <React.StrictMode>
