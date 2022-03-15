3ddea5667847bb6d0d91471014d938d6001d0126
Dan Forder <danfrdr@gmail.com>
2022-03-15 15:16:27 +0000

    removed test route

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import "./styles/main.scss";
 const App = () => {
   return (
     <Routes>
-      <Route path="/test" element={<h1>Hello Test</h1>} />
       <Route path="*" element={<Home />} />
     </Routes>
   );
